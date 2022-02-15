836baa4a9329de306efc52ee16f69ec75db29f3c
Jessica Bahny <77072076+silversun307@users.noreply.github.com>
2022-02-14 16:22:18 -0800

    Add files via upload

--- src/GardenHelper.java
+++ src/GardenHelper.java
@@ -4,13 +4,10 @@ public class GardenHelper {
     GardenPlant daisy, carrot, rose, swissChard, begonia;
     GardenRow one, two, three;
 
-    /**
-     * Runs the Application GardenHelper
-     */
     public void go() {
         view.printMenu();
         String action = view.getAction();
-        while(!action.startsWith("-1")) {  // interject an error, and let student debug - it is a Tuesday lab after all.
+        while(!action.startsWith("n")) {
             if(action.startsWith("a")) {
                 int row = view.getRow();
                 int pH = Integer.parseInt(view.input("What is the pH of the row? "));
@@ -26,41 +23,16 @@ public class GardenHelper {
             action = view.getAction();
         }
     }
-
-    /**
-     * Initializes GardenRow one, two, or three.
-     * <p>Implementation notes:</p>
-     * <ul>
-     *     <li>Creates a new row object based on parameters</li>
-     *     <li>Stores row object in GardenRow one, two, or three according to proper parameter</li>
-     * </ul>
-     * @return true if adding row was successful, return false if unsuccessful
+    /*
+       BRAINSTORM FOR ADDROW() HERE
      */
     public boolean addRow(int row, int pH, String light) {
         //TODO student
-        if(row == 1){
-            one = new GardenRow(light, pH);
-            return true;
-        }
-        else if(row == 2){
-            two = new GardenRow(light, pH);
-            return true;
-        }
-        else if(row == 3){
-            three = new GardenRow(light, pH);
-            return true;
-        }
-        else{
-            return false;
-        }
+        return false;
     }
-
-    /**
-     * Adds plant (based on the five options) to designated row. If plant needs meet row conditions,
-     * the plant will be added. If not, then the plant will not be added to the row.
-     * @return true if plant was successfully added, false if unsuccessful
+    /*
+        DESCRIPTION OF ADDPLANT() HERE
      */
-    // have student post self-explanation for this method? or above method?
     public boolean addPlant(String plantName, int row) {
         if(canPlant(plantName, searchRow(row))){
             searchRow(row).setPlant(searchPlant(plantName));
@@ -73,43 +45,13 @@ public class GardenHelper {
         }
     }
 
-
-    /**
-     * Checks if a particular plant is allowed to be planted where the user designates.
-     *     <p>Conditions for planting:</p>
-     *     <ul>
-     *         <li>Row must have correct sun coverage (full sun, partial sun, or any)</li>
-     *         <li>Row must have a pH between the plant's pH range</li>
-     *         <li>At least one row must have vegetables, so if it is the third row and the other two have flowers - can't plant a flower.
-     *         Also remember that if there is currently no plant in the row, {@link GardenRow#getPlant()} will throw a null pointer exception.
-     *         To remedy this, make the first condition check if {@link GardenRow#getPlant()} != null</li>
-     *     </ul>
-     * Using the provided searchPlant() method may be helpful.
-     * @param plantName name of the pant to be planted in the entire row
-     * @param row row to plant plants in (1, 2, or 3)
-     * @return
-     */
-    // two parts - first write the logic as pseudo code
+    //STEP 3
     public boolean canPlant(String plantName, GardenRow row){
         //TODO student
-        if(searchPlant(plantName).getAmountOfSunshine().equals(row.getLight())){
-            if(searchPlant(plantName).getpHLow() <= row.getPH() && searchPlant(plantName).getpHHigh() >= row.getPH()){
-                if(((one != null && (one.getPlant() != null) && (one.getPlant().getType().equals("VEG")))
-                        || (two != null && two.getPlant() != null && two.getPlant().getType().equals("VEG"))
-                        || (three != null && three.getPlant() != null && three.getPlant().getType().equals("VEG")))){
-                    return true;
-                }
-                if(searchPlant(plantName).getType().equals("VEG")){
-                    return true;
-                }
-            }
-        }
         return false;
     }
 
-    /**
-     * Initializes the possible GardenPlants.
-     */
+
     public void buildOptions() {
         daisy = new GardenPlant("daisy", 6, 8, "any", "FLOWER");
         carrot = new GardenPlant("carrot", 6, 7, "full sun", "VEG");
@@ -118,11 +60,7 @@ public class GardenHelper {
         begonia = new GardenPlant("begonia", 5, 6, "partial sun", "FLOWER");
     }
 
-    /**
-     * Searches for a GardenPlant based on a plant name passed in
-     * @param plantName name of plant
-     * @return one of the GardenPlant options
-     */
+
     public GardenPlant searchPlant(String plantName){
         if(plantName.equals(daisy.getName())){
             return daisy;
@@ -141,10 +79,7 @@ public class GardenHelper {
         }
     }
 
-    /**
-     * Finds the GardenRow based on an numerical input
-     * @param row int that numerically represents the row
-     */
+
     public GardenRow searchRow(int row){
         if(row == 1){
             return one;
